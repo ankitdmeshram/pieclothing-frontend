11f2abaed80bb7683e5ab0905b07ac812a029a36
Ankit Meshram <ankitdm69@gmail.com>
2023-11-16 23:18:27 +0530

    admin cart delete functionality updated and cart page visuals

--- app/routes/admin.cart.tsx
+++ app/routes/admin.cart.tsx
@@ -37,7 +37,7 @@ const adminCart = () => {
     const response: any = await deleteCartById(id);
     if (response.success) {
       setCartDetails((prev: any) => {
-        const cartU = prev.filter((p:any) => p._id != id);
+        const cartU = prev.filter((p: any) => p._id != id);
         return cartU;
       });
       alert("succes");
@@ -54,47 +54,55 @@ const adminCart = () => {
           <AdminHeader />
           <div className="dash-body">
             <div className="dash-content">
-              <table className="pro-table" border={1}>
-                <thead>
-                  <tr>
-                    <th>Sr. No.</th>
-                    <th>Name</th>
-                    <th>Total Items</th>
-                    <th>Created At</th>
-                    <th>Updated At</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cartDetails?.length > 0
-                    ? cartDetails.map((cart: any, i) => {
-                        return (
-                          <tr key={cart?._id}>
-                            <td>{i + 1}</td>
-                            <td>
-                              {cart?.userDetails
-                                ? cart.userDetails.firstName +
-                                  " " +
-                                  cart.userDetails.lastName
-                                : "Unknown"}
-                            </td>
-                            <td>{cart?.totalQuantity}</td>
-                            <td>{cart?.created_date}</td>
-                            <td>{cart?.updated_date}</td>
-                            <td width={100}>
-                              <button
-                                className="delete"
-                                onClick={() => deleteFromCart(cart?._id)}
-                              >
-                                Delete
-                              </button>
-                            </td>
-                          </tr>
-                        );
-                      })
-                    : null}
-                </tbody>
-              </table>
+              <div className="table">
+                <table className="pro-table" border={1}>
+                  <thead>
+                    <tr>
+                      <th>Sr. No.</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Total Items</th>
+                      <th>Created At</th>
+                      <th>Updated At</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartDetails?.length > 0
+                      ? cartDetails.map((cart: any, i) => {
+                          return (
+                            <tr key={cart?._id}>
+                              <td>{i + 1}</td>
+                              <td>
+                                {cart?.userDetails
+                                  ? cart.userDetails.firstName +
+                                    " " +
+                                    cart.userDetails.lastName
+                                  : "Unknown"}
+                              </td>
+                              <td>
+                                {cart?.userDetails?.email
+                                  ? cart?.userDetails?.email
+                                  : "Unknown"}
+                              </td>
+                              <td>{cart?.totalQuantity}</td>
+                              <td>{cart?.created_date}</td>
+                              <td>{cart?.updated_date}</td>
+                              <td width={100}>
+                                <button
+                                  className="delete"
+                                  onClick={() => deleteFromCart(cart?._id)}
+                                >
+                                  Delete
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })
+                      : null}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
