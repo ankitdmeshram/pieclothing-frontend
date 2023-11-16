80c97fd8c4c1bdafa948446604c4e1232d4511c5
Ankit Meshram <ankitdm69@gmail.com>
2023-11-17 00:29:37 +0530

    delete user functionality updated

--- app/routes/admin.users.tsx
+++ app/routes/admin.users.tsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
 import AdminHeader from "~/component/adminHeader";
 import AdminSidebar from "~/component/adminSidebar";
 import isadmin from "~/component/isadmin";
 import styles from "../styles/admin.css";
+import { allUserData, deleteUserById } from "~/controllers/userController";
 
 const adminUsers = () => {
   isadmin();
+
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    allUser();
+  }, []);
+
+  const allUser = async () => {
+    const response: any = await allUserData();
+    if (response?.success) {
+      console.log(response);
+      setUserList(response?.userData);
+    }
+  };
+
+  const deleteUser = async (id: any) => {
+    const response: any = await deleteUserById(id);
+    if (response.success) {
+      console.log(response);
+
+      setUserList((user: any) => {
+        let userData = user.filter((u: any) => u._id != id);
+        return userData;
+      });
+
+      alert("deleted");
+    } else {
+      alert("Error");
+    }
+  };
+
   return (
     <>
       <div className="dash-container">
@@ -18,14 +51,40 @@ const adminUsers = () => {
                   <tr>
                     <th>Sr. No.</th>
                     <th>Name</th>
-                    <th>Price</th>
+                    <th>Email</th>
+                    <th>Phone</th>
                     <th>Created At</th>
-                    <th>Updated At</th>
                     <th>Delete</th>
-                    <th>Update</th>
                   </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                  {userList.length > 0
+                    ? userList.map((user: any, i) => {
+                        return (
+                          <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{user?.firstName + " " + user?.lastName}</td>
+                            <td>{user?.email}</td>
+                            <td>{user?.phone}</td>
+                            <td> {user?.created_at}</td>
+                            <td>
+                              <button
+                                className="delete"
+                                onClick={() => {
+                                  let sure = confirm(
+                                    "Are you sure you want to delete?"
+                                  );
+                                  sure && deleteUser(user?._id);
+                                }}
+                              >
+                                Delete
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    : null}
+                </tbody>
               </table>
             </div>
           </div>
